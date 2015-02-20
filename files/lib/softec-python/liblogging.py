de6efad471e728f281ac24608f182e61bfc07ba3
Lorenzo Cocchi <lorenzo.cocchi@softecspa.it>
2015-02-20 15:50:47 +0100

    update liblogging de no attri

--- files/lib/softec-python/liblogging.py
+++ files/lib/softec-python/liblogging.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python
 
 import logging
+import logging.handlers
 import sys
 
-# http://docs.python.org/2/howto/logging.html
+# https://docs.python.org/2.7/howto/logging-cookbook.html
 # parte del codice su gentile concessione di Andrea Cappelli
+# TODO: logging.handlers.TimedRotatingFileHandler
 
 __version__ = (0, 0, 1)
 __author__ = 'Lorenzo Cocchi <lorenzo.cocchi@softecspa.it>'
 
 
-def setup_logging(name=None, filename=False, log_level='INFO', fmt=None,
-                  datefmt=None):
+def setup_logging(name=None, filename=None, filemode='a', log_level='INFO',
+                  fmt=None, datefmt=None, rotate=False, rotate_max_bytes=1024,
+                  rotate_backup_count=10):
     """
 
     Level       Numeric value
@@ -23,31 +26,50 @@ def setup_logging(name=None, filename=False, log_level='INFO', fmt=None,
     NOTSET      0
 
     """
-    log = logging.getLogger(name)
+    logger = logging.getLogger(name)
 
-    if not fmt:
+    if fmt is None:
         fmt = '%(asctime)s [%(levelname)s] %(name)s.%(module)s: %(message)s'
 
     formatter = logging.Formatter(fmt, datefmt)
 
-    if not filename:
-        hdlr = logging.StreamHandler(sys.stdout)
+    if filename is True or filename is False:
+        raise ValueError('filename should not be boolean')
+
+    if filename is None:
+        # sys.stderr
+        handler = logging.StreamHandler()
     elif filename == '-':
-        # log to sys.stderr
-        hdlr = logging.StreamHandler()
-    elif filename:
-        hdlr = logging.FileHandler(filename)
+        # sys.stdout
+        handler = logging.StreamHandler(sys.stdout)
+    elif filename and filename != '-':
+        if rotate is False:
+            handler = logging.FileHandler(filename, mode=filemode)
+        else:
+            handler = logging.handlers.RotatingFileHandler(
+                filename,
+                mode=filemode,
+                maxBytes=rotate_max_bytes,
+                backupCount=rotate_backup_count
+            )
     else:
-        hdlr = logging.NullHandler()
+        handler = logging.NullHandler()
 
-    hdlr.setFormatter(formatter)
-    log.addHandler(hdlr)
-    log.setLevel(getattr(logging, log_level))
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.setLevel(getattr(logging, log_level))
 
-    return log
+    return logger
 
 
 if __name__ == '__main__':
-    log = setup_logging(name=__name__, log_level='DEBUG')
-    for level in ('debug', 'info', 'warn', 'error', 'critical'):
-        getattr(log, level)('foobar')
+    log_levels = ('debug', 'info', 'warn', 'error', 'critical')
+    log_stdout = setup_logging(name='%s.%s' % (__name__, 'stdout'),
+                               log_level='DEBUG',
+                               filename='-')
+    log_stderr = setup_logging(name='%s.%s' % (__name__, 'stderr'),
+                               log_level='ERROR')
+    for level in log_levels:
+        getattr(log_stdout, level)('foobar')
+    for level in log_levels:
+        getattr(log_stderr, level)('foobar')
